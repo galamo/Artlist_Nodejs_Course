88623a7b2e77274743b969c755bb5b5cfd78aba9
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-09-08 18:04:21 +0300

    llibs

---
+++ libs/is-valid-seq/index.js
@@ -0,0 +1,13 @@
+// 0 1 2 3
+// [1,2,3,4] => [1,3] OR [1,4] Valid
+function isValidSeq(arr, seq) {
+    const seqCopy = [...seq]
+    for (let index = arr.length - 1; index >= 0; index--) {
+        if (seqCopy[seqCopy.length - 1] === arr[index]) {
+            seqCopy.pop();
+        }
+    }
+    return !seq.length
+}
+
+module.exports = { isValidSeq }
