6a3675ab354813d4be7f3f7bf704d19af63eea39
Gal Amouyal <galamouy@gals-mbp.trusteer.il.ibm.com>
2022-09-14 13:43:01 +0300

    unexpectedBehavior

--- examples/nodeSingleThread/index.js
+++ examples/nodeSingleThread/index.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto")
+const { readFile, writeFile, readFileSync, writeFileSync } = require("fs")
 const { init } = require("../fetchApiData")
 // const start = Date.now()
 // for (let index = 0; index < 999999999; index++) {
@@ -6,23 +7,37 @@ const { init } = require("../fetchApiData")
 // console.log(Date.now() - start)
 // function init() { }
 // init();
-process.env.UV_THREADPOOL_SIZE = 6 // Change it if you brave enought
+process.env.UV_THREADPOOL_SIZE = 4 // Change it if you brave enought
+
 const start = Date.now()
-crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
-    console.log("1:", Date.now() - start)
+
+function hashing() {
+    crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
+        console.log("1:", Date.now() - start)
+    })
+    crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
+        console.log("2:", Date.now() - start)
+    })
+    crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
+        console.log("3:", Date.now() - start)
+    })
+    crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
+        console.log("4:", Date.now() - start)
+    })
+    crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
+        console.log("5:", Date.now() - start)
+    })
+
+}
+console.log("Start reading file...")
+readFile("./index.js", "utf-8", () => {
+    console.log("readFile:", Date.now() - start)
 })
-crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
-    console.log("2:", Date.now() - start)
-})
-crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
-    console.log("3:", Date.now() - start)
-})
-crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
-    console.log("4:", Date.now() - start)
-})
-crypto.pbkdf2("password", "salta", 100000, 512, "sha512", (error, result) => {
-    console.log("5:", Date.now() - start)
-})
+const file1 = readFileSync("./index.js", "utf-8")
+console.log("readFileSync:", Date.now() - start)
+
+hashing();
+
 
 // Exmpale of async operation
-init()
+// init()
