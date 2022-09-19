db05d3420ad829f107eaad8eaac546f10fd8b978
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-09-19 16:41:24 +0300

    input_validation_exted

--- apps/countries-api/src/countries/index.ts
+++ apps/countries-api/src/countries/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express"
 import axios from "axios";
+import * as joi from "joi"
 import getFlags from "./handler/getCountries";
 import getCountryTemplate from "./handler/getCountryTemplate";
 const router = express.Router();
@@ -15,7 +16,7 @@ router.get("/", async (req: Partial<express.Request>, res: express.Response) => 
     }
 })
 
-router.get("/template", async (req: Partial<express.Request>, res: express.Response, next) => {
+router.get("/template", validate, async (req: Partial<express.Request>, res: express.Response, next) => {
     try {
         const result = await getCountryTemplate()
         res.json(result)
@@ -27,3 +28,43 @@ router.get("/template", async (req: Partial<express.Request>, res: express.Respo
 
 
 export { router }
+
+export const searchSchema = joi.object({
+    query: joi.object({
+        search: joi.string().required()
+    })
+})
+
+
+const schemas = {
+    "/countries/template": searchSchema,
+
+    // "/users": usersSchema
+}
+
+export function getSchemaValidation(path) {
+    const current = schemas[path]
+    return current
+}
+
+export function validate(req, res, next) {
+    const schemaValidation = getSchemaValidation(req.originalUrl.split("?")[0])
+    console.log(req.originalUrl)
+    const { error } = schemaValidation.validate({ query: req.query });
+    if (error) {
+        return next({ ...new Error(), isBadReuqest: true })
+    }
+    return next();
+}
+
+export function validate2(path) {
+    return async function (req, res, next) {
+        const schemaValidation = getSchemaValidation(path)
+        console.log(path)
+        const { error } = schemaValidation.validate({ query: req.query });
+        if (error) {
+            return next({ ...new Error(), isBadReuqest: true })
+        }
+        return next();
+    }
+}
